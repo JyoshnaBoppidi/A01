ca924c097dd86239b6a76945360d27125b6b68c5
Boppidi Jyoshna <s533706@nwmissouri.edu>
2018-10-07 23:04:58 -0500

    commit

--- scores.js
+++
@@ -1,9 +0,0 @@
-function scoresIncreasing(scores) {
-let scores1 = parseInt(scores)
-  for (var i = 1; i < scores1.length; i++){
-    if (scores1[i] != scores1[i-1] && scores1[i] < scores1[i-1]){
-      alert("false");
-    }
-  }
-  alert("true");
-}
